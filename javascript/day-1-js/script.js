bbf2af9d2e7895ff1053f19794d9ef9284db94dd
Afroj <overthinker.ltd03@gmail.com>
2026-05-27 12:32:07 +0530

    JS day-1: basics, alert, and prompt

---
+++ javascript/day-1-js/script.js
@@ -0,0 +1,12 @@
+
+
+// your first function!
+function sayHello() {
+    alert("Hello World! 🔥");
+    confirm("Are you sure?");
+    prompt("What is your name?");
+}
+
+let name = prompt("What is your name?");
+
+alert("Ohhh nice to meet you " + name + "! (￣y▽￣)╭ Ohohoho.....");
